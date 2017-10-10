d71a22c1bb829511ffaa0eae0bdecb1cf20a00c7
Holly Ansel <hollya3643@gmail.com>
2017-10-10 02:08:22 +0000

    adds complete handmade maze

--- script.js
+++ script.js
@@ -41,11 +41,52 @@ $(document).ready(function(){
         $("#maze1").append('<a-cylinder src="#marble"  static-body color="white"  height="'+ height + '" radius="' + radius +'"  position="'+ startX+' '+ centerY+' '+ startZ+'"></a-cylinder ');
     }
     
-    makeWall(0, 0, 0, -2);
-    makeWall(1, 0, 1, -1);
-    makeWall(1, -1, 2, -1);
-    makeWall(2, -1, 2, -3);
-    makeWall(0, -2, 1, -2);
-    makeWall(1, -2, 1, -3);
+    makeWall(0, 0, 0, -3);
+    makeWall(1, 0, 8, 0);
+    makeWall(1, -1, 5, -1);
+    makeWall(1, -2, 2, -2);
+    makeWall(2, -2, 2, -3);
+    makeWall(2, -2, 4, -2);
+    makeWall(0, -3, 1, -3);
+    makeWall(0, -3, 0, -7);
+    makeWall(2, -3, 5, -3);
+    makeWall(5, -1, 5, -4);
+    makeWall(5, -3, 6, -3);
+    makeWall(5, -1, 7, -1);
+    makeWall(7, -1, 7, -2);
+    makeWall(7, -1, 6, -2);
+    makeWall(7, -2, 7, -3);
+    makeWall(7, -3, 7, -4);
+    makeWall(7, -3, 9, -3);
+    makeWall(8, -2, 9, -2);
+    makeWall(8, 0, 8, -1);
+    makeWall(8, 0, 9, 0);
+    makeWall(9, 0, 9, -1);
+    makeWall(6, -4, 7, -4);
+    makeWall(2, -4, 4, -4);
+    makeWall(4, -4, 4, -5);
+    makeWall(4, -5, 6, -5);
+    makeWall(3, -5, 4, -5);
+    makeWall(1, -3, 1, -5);
+    makeWall(1, -5, 3, -5);
+    makeWall(0, -7, 5, -7);
+    makeWall(6, -7, 10, -7);
+    makeWall(9, -2, 9, -3);
+    makeWall(9, -3, 9, -4);
+    makeWall(9, -4, 10, -4);
+    makeWall(10, 0, 10, -7);
+    makeWall(1, -6, 1, -7);
+    makeWall(2, -6, 3, -6);
+    makeWall(3, -5, 3, -6);
+    makeWall(4, -6, 6, -6);
+    makeWall(6, -5, 6, -6);
+    makeWall(8, -4, 8, -5);
+    makeWall(7, -5, 7, -7);
+    makeWall(9, -5, 10, -5);
+    makeWall(9, -6, 10, -6);
+    makeWall(9, -6, 9, -7);
+    makeWall(8, -6, 8, -7);
+    
+    
     
 });    
